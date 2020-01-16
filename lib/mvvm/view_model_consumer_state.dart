196821f6795e8594eeee3babefe550a6821b1f17
Felix Junghans <kontakt@felixjunghans.de>
2020-01-16 09:46:11 +0100

    Extend state with new postInit function

--- lib/mvvm/view_model_consumer_state.dart
+++ lib/mvvm/view_model_consumer_state.dart
@@ -10,11 +10,15 @@ abstract class ViewModelConsumerState<T extends StatefulWidget,
   @override
   void initState() {
     super.initState();
-    init();
+
+    postInit(() {
+      T2 viewModel = Provider.of<T2>(context);
+      init(viewModel);
+    });
   }
 
   @protected
-  void init();
+  void init(T2 viewModel);
 
   @protected
   Widget builder(BuildContext context, T2 viewModel, Widget child);
@@ -23,5 +27,19 @@ abstract class ViewModelConsumerState<T extends StatefulWidget,
     return Consumer<T2>(
       builder: builder,
     );
+  }
+}
+
+extension StateExtension<T extends StatefulWidget> on State<T> {
+  Stream<bool> waitForStateLoading() async* {
+    while (!mounted) {
+      yield false;
+    }
+    yield true;
+  }
+
+  Future<void> postInit(VoidCallback action) async {
+    await for (bool isLoaded in waitForStateLoading()) {}
+    action();
   }
 }
